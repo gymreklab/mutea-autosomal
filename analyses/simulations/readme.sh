bd32b7a02c2785000cdd8c0bb3bb2fbe3f18eb9a
Melissa Ann Gymrek <mgymrek@comet-ln3.sdsc.edu>
2016-11-17 21:37:16 -0800

    updating joint to run on xsede

--- analyses/simulations/readme.sh
+++ analyses/simulations/readme.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
+source params_base.sh
 
 ############# Per locus ############
 # Run simulations for each condition and get estimates
 ./run_simulate_locus.sh params.sh
 ./run_simulate_locus.sh params_withstutter.sh
-
-# Get number of mutations
-./get_num_mutations.sh params.sh
-cat tree_mutinfo.tab | grep -v chrom | intersectBed -a stdin -b tree_truth.bed -wa -wb | awk '{print $5+$6 "\t" $14}' | sort -g -k 2 | datamash -g 2 median 1
 
 # Calibrate standard errors
 ./calibrate_errors.sh params.sh
@@ -16,4 +13,4 @@ cat tree_mutinfo.tab | grep -v chrom | intersectBed -a stdin -b tree_truth.bed -
 ############# Joint ############
 ./run_simulate_locus.sh params_joint.sh
 ./run_joint.sh params_joint.sh
-./combine_results.sh params_joint.sh | awk -F" " '(NF==8)' | sed 's/ loci//' > joint_estimates.tab
+./combine_results.sh params_joint.sh | awk -F" " '(NF==8)' | sed 's/ loci//' > ${BASEDIR}/simulations/joint_estimates.tab
