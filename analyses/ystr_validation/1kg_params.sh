abaac9eb28f6738120458cdbd6112bde4e117b78
mgymrek <mgymrek@mit.edu>
2016-11-18 00:58:17 -0500

    updating paths to ystr vcfs

--- analyses/ystr_validation/1kg_params.sh
+++ analyses/ystr_validation/1kg_params.sh
@@ -2,7 +2,8 @@
 source params_base.sh
 
 MUTEADIR=${HOME}/workspace/mutea-autosomal/
-VCF=/groups/reich/melissa/mutation_rate_project/validation/fromThomas/final_callset.sorted.vcf.gz
+#VCF=/groups/reich/melissa/mutation_rate_project/validation/fromThomas/final_callset.sorted.vcf.gz
+VCF=/oasis/projects/nsf/ddp268/mgymrek/mutea-autosomal/ystr_validation/data/final_callset.sorted.vcf.gz
 SAMPLEPAIRS=1kg_sample_pairs.tab
 OUTPREFIX=ystrs_1kg
 LOCI=ystr_coords_forest_chr.bed
--- analyses/ystr_validation/sgdp_params.sh
+++ analyses/ystr_validation/sgdp_params.sh
@@ -2,7 +2,8 @@
 source params_base.sh
 
 MUTEADIR=${HOME}/workspace/mutea-autosomal/
-VCF=/groups/reich/melissa/mutation_rate_project/str_genotypes/round2/sgdp_memstrs_Y.filt.sorted.vcf.gz
+#VCF=/groups/reich/melissa/mutation_rate_project/str_genotypes/round2/sgdp_memstrs_Y.filt.sorted.vcf.gz
+VCF=/oasis/projects/nsf/ddp268/mgymrek/mutea-autosomal/ystr_validation/data/sgdp_memstrs_Y.filt.sorted.vcf.gz
 SAMPLEPAIRS=sgdp_sample_pairs.tab
 OUTPREFIX=ystrs_sgdp
 LOCI=ystr_coords_forest.bed
