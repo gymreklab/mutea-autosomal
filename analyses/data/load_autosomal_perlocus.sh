a4f0eb32fb3f83dc23badbdd58a73d0f9a3d7db4
mgymrek <mgymrek@mit.edu>
2016-11-19 17:13:34 -0500

    adding load data for autosomal perlocus

---
+++ analyses/data/load_autosomal_perlocus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source params.sh
+
+scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/perlocus/autosomal_estimates_ml.bed.gz ${DATADIR}/autosomal_estimates/perlocus/
+scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/perlocus/autosomal_stutter_ml.bed.gz ${DATADIR}/autosomal_estimates/perlocus/
+scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/perlocus/autosomal_estimates_ml_filtered.bed.gz ${DATADIR}/autosomal_estimates/perlocus/
+
