a34aa721ff86d1613132439a4525643da3e2a5a9
mgymrek <mgymrek@mit.edu>
2016-11-21 00:29:48 -0500

    adding command to run model notebook

--- analyses/constraint/readme.sh
+++ analyses/constraint/readme.sh
@@ -3,11 +3,15 @@
 ##############################################################
 # Get genome-wide file with of features + observed rates
 ./get_features.sh params.sh
-
 ./combine_features_estimates.sh params.sh
 ##############################################################
 
 ##############################################################
 # Get set of loci to train on
 ./get_training_loci.sh params.sh
+##############################################################
+
+##############################################################
+# Build model
+runipy BuildConstraintModel.ipynb
 ##############################################################
