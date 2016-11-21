5e32cbcaa5880f060c1c1b08f02a8ee6e35b7327
mgymrek <mgymrek@mit.edu>
2016-11-21 00:21:02 -0500

    adding gzip to combine features

--- analyses/constraint/combine_features_estimates.sh
+++ analyses/constraint/combine_features_estimates.sh
@@ -5,9 +5,10 @@ source $PARAMFILE
 
 tmpdir=$(mktemp -d -p ${TMPLOC})
 
-cat ${BASEDIR}/constraint/lobSTR_ref_GRCh37_properties_filtered.tab | grep -v chrom | \
+zcat ${BASEDIR}/constraint/lobSTR_ref_GRCh37_properties_filtered.tab | grep -v chrom | \
     intersectBed -a ${BASEDIR}/autosomal_estimates/perlocus/autosomal_estimates_ml_filtered.bed.gz -b stdin -wa -wb | \
     cut -f 10-12 --complement > ${tmpdir}/tmp
 echo "chrom,start,end,ml_mu,ml_beta,ml_p,ml_mu_stderr,numsamples,strfilter,motif,length,uninterrupted_length,recomb,gc,entropy,reptiming,featurefilter" | \
     sed 's/,/\t/g' > ${BASEDIR}/constraint/autosomal_perlocus_observed.bed
 cat ${tmpdir}/tmp >> ${BASEDIR}/constraint/autosomal_perlocus_observed.bed
+gzip ${BASEDIR}/constraint/autosomal_perlocus_observed.bed
