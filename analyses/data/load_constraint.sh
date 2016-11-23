aa2fd64463b0472c3004a8d442f08c5b46111df9
mgymrek <mgymrek@mit.edu>
2016-11-22 17:42:39 -0800

    adding zscore dist

--- analyses/data/load_constraint.sh
+++ analyses/data/load_constraint.sh
@@ -3,3 +3,5 @@
 source params.sh
 
 scp ${HOST}:${HOSTBASEDIR}/constraint/lobSTR_ref_GRCh37_properties_filtered.tab.gz ${DATADIR}/constraint/
+scp ${HOST}:${HOSTBASEDIR}/constraint/autosomal_perlocus_estimates.bed ${DATADIR}/constraint/
+
