e02e92f37f26448ff52d9d1a80d5b93f30b1c823
mgymrek <mgymrek@mit.edu>
2016-12-08 18:04:08 -0800

    adding supp data, preparing figures for submission

--- analyses/data/load_autosomal_perlocus.sh
+++ analyses/data/load_autosomal_perlocus.sh
@@ -2,7 +2,9 @@
 
 source params.sh
 
-scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/perlocus/autosomal_estimates_ml.bed.gz ${DATADIR}/autosomal_estimates/perlocus/
-scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/perlocus/autosomal_stutter_ml.bed.gz ${DATADIR}/autosomal_estimates/perlocus/
-scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/perlocus/autosomal_estimates_ml_filtered.bed.gz ${DATADIR}/autosomal_estimates/perlocus/
+#scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/perlocus/autosomal_estimates_ml.bed.gz ${DATADIR}/autosomal_estimates/perlocus/
+#scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/perlocus/autosomal_stutter_ml.bed.gz ${DATADIR}/autosomal_estimates/perlocus/
+#scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/perlocus/autosomal_estimates_ml_filtered.bed.gz ${DATADIR}/autosomal_estimates/perlocus/
+
+scp ${HOST}:${HOSTBASEDIR}/autosomal_estimates/*v2*.gz ${DATADIR}/autosomal_estimates/perlocus/
 
--- analyses/data/load_constraint.sh
+++ analyses/data/load_constraint.sh
@@ -3,5 +3,7 @@
 source params.sh
 
 scp ${HOST}:${HOSTBASEDIR}/constraint/lobSTR_ref_GRCh37_properties_filtered.tab.gz ${DATADIR}/constraint/
-scp ${HOST}:${HOSTBASEDIR}/constraint/autosomal_perlocus_estimates.bed ${DATADIR}/constraint/
+scp ${HOST}:${HOSTBASEDIR}/constraint/autosomal_perlocus_train_intergenic.bed.gz ${DATADIR}/constraint/
+scp ${HOST}:${HOSTBASEDIR}/constraint/autosomal_perlocus_observed.bed.gz ${DATADIR}/constraint/
+#scp ${HOST}:${HOSTBASEDIR}/constraint/autosomal_perlocus_estimates.bed ${DATADIR}/constraint/
 
